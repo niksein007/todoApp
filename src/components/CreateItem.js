ec7ff15dba61fe2690629d5be986632e7eaeeb14
niksein007 <markpearceniksein@gmail.com>
2020-06-25 22:43:33 +0100

    modified the menu

--- src/components/CreateItem.js
+++ src/components/CreateItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"; 
 
 class CreateItem extends React.Component {
   state = {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,68 @@
-import React from 'react'
+import React from "react";
 
-class Header extends React.Component{
-    render(){
-    return(
-        <header className='Header'>
-           <div className='title'>markList</div>
-           <div className='menu-burger'></div>
+class Header extends React.Component {
+  state = {
+    active: true,
+  };
+
+  navHandler = () => {
+   let nav = document.querySelector("nav");
+    // console.log(nav);
+    nav.classList.toggle('visible')
+    
+  };
+
+  listHandler1=()=>{
+    const element= document.getElementById('inner1')
+    // console.log(element);
+    element.classList.toggle('visible')
+  }
+  
+  listHandler2=()=>{
+    const element= document.getElementById('inner2')
+    // console.log(element);
+    element.classList.toggle('visible')
+  }
 
 
-        </header>
-    )
+  render() {
+    return (
+      <header className="Header">
+        <div className="title">markList</div>
+        <div onClick={this.navHandler}>
+        <div className="menu-burger"  ></div>
+
+        </div>
+
+        <nav>
+        <ol className='list'>
+            <li  className='list-item' onClick={this.listHandler1}>
+              Plans
+              <ol className='list inner-list' id='inner1'>
+                <li className='list-item inner-list-item'>Daily</li>
+                <li className='list-item inner-list-item'>weekly</li>
+                <li className='list-item inner-list-item'>Monthly</li>
+                <li className='list-item inner-list-item'>Yearly</li>
+              </ol>
+            </li>
+            <li  className='list-item' onClick={this.listHandler2}>
+              Personal Quotes
+              <ol className='list inner-list' id='inner2' >
+                <li className='list-item inner-list-item'>Love</li>
+                <li className='list-item list-item inner-list-item'>Life</li>
+                <li className='list-item inner-list-item'>Friends</li>
+                <li className='list-item inner-list-item'>Sex</li>
+              </ol>
+            </li>
+            <li className='list-item' >About Marklist</li>
+            <li className='list-item' >Contact us</li>
+
+            <li className='list-item' >Log In</li>
+            <li className='list-item' >Log Out</li>
+          </ol>
+        </nav>
+      </header>
+    );
+  }
 }
-}
-export default Header
+export default Header;
